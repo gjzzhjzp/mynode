09ffc10cbaf5224c29656e7bc541aae7a5461c42
18380446946 <872610221@qq.com>
2025-04-15 17:17:14 +0800

    数据库账号修改

--- config/config.default.js
+++ config/config.default.js
@@ -35,7 +35,7 @@ module.exports = appInfo => {
     client: {
       host: '47.108.224.146',     // MySQL 地址
       port: '3306',          // 端口
-      user: 'root',          // 用户名
+      user: 'root2',          // 用户名
       password: '123456', // 密码
       database: 'notebook', // 数据库名
     },
